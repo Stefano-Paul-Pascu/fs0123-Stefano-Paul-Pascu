1ceb9f735bdeae7c718ddc23934df2bbb92abc94
Stefano-Paul-Pascu <stefanopascu@gmail.com>
2023-03-13 17:19:13 +0100

    commit

---
+++ m2/s3/g1/pet.js
@@ -0,0 +1,12 @@
+let Pet = function(petName, ownerName, species, breed){
+    this.petName = petName;
+    this.ownerName = ownerName;
+    this.species = species;
+    this.breed = breed;
+}
+
+let Rex = new Pet (Rex, Luigi, cane, pastoreTedesco);
+
+let Tigro = new Pet (Tigro, Luigi, gatto, soriano);
+
+let Titty = new Pet (Titty, Marta, uccello, canarino);
---
+++ m2/s3/g1/script.js
@@ -0,0 +1,16 @@
+let User = function (firstName, lastName, age, location) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.location = location;
+}
+
+let Marco = new User ('Marco', 'Rossi', 32, 'Roma');
+console.log(Marco);
+
+let Maria = new User ('Maria', 'Verdi', 28, 'Milano');
+console.log(Maria);
+
+function comparazione(){
+    
+}
